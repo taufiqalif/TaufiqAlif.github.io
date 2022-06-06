1c80c5da99d11d839d4b4853b2fa34b8f4ad3664
taufiqalif <aliftaufiq4@gmail.com>
2022-06-06 12:08:41 +0700

    portfolio tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {},
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
